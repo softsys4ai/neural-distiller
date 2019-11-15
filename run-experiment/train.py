43fe6311f6ef474a9b951cd3b448858af9ca4eaf
Blake Edwards <blakeedwards823@gmail.com>
2019-11-15 17:20:18 -0500

    targeted GPU training added

--- run-experiment/train.py
+++ run-experiment/train.py
@@ -1,4 +1,7 @@
 import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
+# The GPU id to use, usually either "0" or "1";
+os.environ["CUDA_VISIBLE_DEVICES"]="2";
 import socket
 import logging
 import sys
