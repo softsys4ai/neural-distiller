f0ef1b51d6a642fdd32fcac556e1c50b0e234428
Stephen Baione <sbaione@email.sc.edu>
2020-03-19 22:28:09 -0400

    Added tf official models, support for low magnitude pruning. Need to look further into results. Numbers are off.

--- src/pruning/prune_test.py
+++ src/pruning/prune_test.py
@@ -4,77 +4,58 @@
 # TODO:// After POF, start using VGG16 and ResNet50 for pruning
 """
 
-from pruning.prune_wrapper import PruneWrapper
-from pruning.ranker import Ranker
+from pruning.pruner import Pruner
 
 import tensorflow as tf
-import numpy as np
+from tensorflow.python.keras import datasets
 
 from tensorflow.python.keras.layers import Layer, Conv2D, Dense, Flatten, MaxPool2D, Dropout
 from tensorflow.python.keras.models import Model, Sequential
+from models.tf_official_models import official
+from official.r1.mnist import mnist as tf_mnist_model
 
 from tensorflow.python.keras.optimizers import Adam
 from tensorflow.python.keras.losses import SparseCategoricalCrossentropy
 
-from tensorflow.python.keras import datasets
+import numpy as np
 
 
-def load_dataset():
+def load_dataset(batch_size=10000):
     mnist = datasets.mnist
     (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
     X_train, X_test = X_train / 255.0, X_test / 255.0
-    X_train, X_test = np.expand_dims(X_train, axis=-1), np.expand_dims(X_test, axis=-1)
-    (X_train, Y_train), (X_test, Y_test) = (X_train[:10000], Y_train[:10000]), (X_test[:10000], Y_test[:10000])
+    (X_train, Y_train), (X_test, Y_test) = (X_train[:batch_size], Y_train[:batch_size]), \
+                                           (X_test[:batch_size], Y_test[:batch_size])
+    X_train, X_test = np.reshape(X_train, (batch_size, 784)), np.reshape(X_test, (batch_size, 784))
     return (X_train, Y_train), (X_test, Y_test)
 
 
 def load_model():
-    model = Sequential()
-    model.add(Conv2D(8, kernel_size=5, activation='relu', input_shape=(28, 28, 1)))
-    model.add(MaxPool2D())
-    model.add(Conv2D(16, kernel_size=5, activation='relu'))
-    model.add(MaxPool2D())
-    model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(10, activation='softmax'))
-    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    model = tf_mnist_model.create_model("channels_last")
+    model.summary()
     return model
+
+
+def compile_model(model: Model, optimizer="adam", loss="sparse_categorical_crossentropy", metrics=None):
+    if metrics is None:
+        metrics = ["accuracy"]
+    model.compile(optimizer, loss, metrics)
 
 
 def train_model(model, X_train, Y_train, epochs=15):
     model.fit(X_train, Y_train, epochs=epochs)
 
 
-def test_wrapper(model, X_train, Y_train, X_test, Y_test):
-    original_predictions = model(X_test, training=False)
-    wandb = model.get_weights()
-    test_layer = model.layers[1]
-    wrapped_layer = PruneWrapper(test_layer)
-    wrapped_layer.build(test_layer.input_shape)
-    mask = wrapped_layer.get_mask()
-    new_mask_vals = mask.numpy()
-    new_mask_vals[0, :] = 0.0
-    wrapped_layer.set_mask(new_mask_vals)
-    wrapped_layer.prune()
-    layers = [layer for layer in model.layers]
-    layers[1] = wrapped_layer
-    new_model = rebuild_model(model, layers, X_train, Y_train)
-    return model, new_model
-
-
-def rebuild_model(model, layers, X_train, Y_train):
-    new_model = Sequential()
-    for layer in layers:
-        new_model.add(layer)
-    new_model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=['accuracy'])
-    return new_model
-    
-    
 def test():
-    (X_train, Y_train), (X_test, Y_test) = load_dataset()
+    (X_train, Y_train), (X_test, Y_test) = load_dataset(batch_size=10000)
     model = load_model()
-    train_model(model, X_train, Y_train)
+    compile_model(model)
+    train_model(model, X_train, Y_train, epochs=5)
+    pruner = Pruner(model, X_train, Y_train, X_test, Y_test, "weight", "low_magnitude")
+    pruner.prune()
+    evaluation = pruner.evaluate_pruned_model()
+    print(evaluation)
 
 
-
-
+if __name__ == "__main__":
+    test()
--- src/pruning/pruner.py
+++ src/pruning/pruner.py
@@ -27,36 +27,103 @@ import tensorflow as tf
 from tensorflow.python.keras.layers import Layer, Conv2D
 from tensorflow.python.keras.models import Model
 
+import tensorflow_model_optimization as tfmot
+
 from pprint import pprint as pprint
 
 
 class Pruner(object):
-    def __init__(self, model: Model, X_test, Y_test, prune_level: str, prune_method: str):
+    def __init__(self, model: Model, X_train, Y_train, X_test, Y_test, prune_level: str, prune_method: str):
         self.model = model
+        self.pruned_model = None
 
         if prune_level not in get_supported_prune_levels():
             raise ValueError("Unsupported prune_level")
-        if prune_method not in get_supported_prune_methods():
-            raise ValueError("Unsupported prune_method")
+        # if prune_method not in get_supported_prune_methods():
+        #    raise ValueError("Unsupported prune_method")
 
         self.prune_level = prune_level
         self.prune_method = prune_method
         self.ranker = Ranker(model)
+
+        self.X_train = X_train
+        self.Y_train = Y_train
         self.X_test = X_test
         self.Y_test = Y_test
 
     # sparsity is percentage of network params that should remain after prune
-    def prune(self, sparsity: float):
-        params = self.model.count_params()
-        sparsity = int(params * sparsity)
-        ranked_weights = self.rank_weights()
-        pprint(ranked_weights)
-        # TODO:// Prune specified number of weights based on sparsity
+    def prune(self, **kwargs):
+        """
+        Executes pruning method on model
+        :param kwargs:
+            Kwargs will vary depending on prune method chosen. Reference config for supported methods and corresponding
+            keyword arguments
+        :return:
+        """
+
+        def _get_poly_prune_schedule(**kwargs):
+            initial_sparsity = kwargs.get("initial_sparsity", 0.2)
+            final_sparsity = kwargs.get("final_sparsity", 0.8)
+            begin_step = kwargs.get("begin_step", 1000)
+            end_step = kwargs.get("end_step", 2000)
+            return tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=initial_sparsity,
+                                                        final_sparsity=final_sparsity,
+                                                        begin_step=begin_step,
+                                                        end_step=end_step)
+
+        def _prune_low_magnitude(_model, _X_train, _Y_train, _X_test, _Y_test, epochs=10, **kwargs):
+            prune_schedule = None
+            pruning_schedule_type = kwargs.get("pruning_schedule_type", "polynomial_decay")
+
+            # Collecting parameters for low magnitude pruning
+            if pruning_schedule_type == "polynomial_decay":
+                prune_schedule = _get_poly_prune_schedule(**kwargs)
+
+            # Loading model with Prune Wrappers applied
+            model_to_prune: Model = tfmot.sparsity.keras.prune_low_magnitude(_model, prune_schedule)
+            model_to_prune.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+            model_to_prune.summary()
+
+            # Fine tune model
+            callbacks = [tfmot.sparsity.keras.UpdatePruningStep()]
+            model_to_prune.fit(_X_train,
+                               _Y_train,
+                               epochs=epochs,
+                               verbose=1,
+                               callbacks=callbacks,
+                               validation_data=(_X_test, _Y_test))
+
+            _pruned_model = tfmot.sparsity.keras.strip_pruning(model_to_prune)
+            _pruned_model.summary()
+            _pruned_model.compile(optimizer="adam",
+                                  loss="sparse_categorical_crossentropy",
+                                  metrics=["accuracy"])
+            return _pruned_model
+
+        model = self.get_model()
+        assert model is not None
+        pruned_model = self.pruned_model
+
+        (X_train, Y_train), (X_test, Y_test) = (self.X_train, self.Y_train), (self.X_test, self.Y_test)
+        for data in [X_train, Y_train, X_test, Y_test]:
+            assert data is not None
+
+        prune_method = self.get_prune_method()
+        if prune_method == "low_magnitude":
+            pruned_model = _prune_low_magnitude(model, X_train, Y_train, X_test, Y_test, **kwargs)
+
+        self.pruned_model = pruned_model
 
     def rank_weights(self):
         ranker = self.ranker
         ranked_weights = ranker.rank(self.X_test, self.Y_test, self.prune_method, self.prune_level)
         return ranked_weights
+
+    def evaluate_pruned_model(self, verbose=0):
+        pruned_model = self.pruned_model
+        X_test = self.X_test
+        Y_test = self.Y_test
+        return pruned_model.evaluate(X_test, Y_test, verbose=verbose)
 
     def get_model(self):
         return self.model
@@ -72,11 +139,3 @@ class Pruner(object):
 
     def set_prune_method(self, prune_method):
         self.prune_method = prune_method
-
-
-if __name__ == "__main__":
-    (X_train, Y_train), (X_test, Y_test) = prune_test.load_dataset()
-    model = prune_test.load_model()
-    prune_test.train_model(model, X_train, Y_train, epochs=2)
-    pruner = Pruner(model, X_test, Y_test, prune_level="filter", prune_method="taylor_first_order")
-    pruner.prune(sparsity=0.5)
