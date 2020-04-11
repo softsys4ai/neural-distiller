3a8d60852539166b6053a0815ac87ffdc28b65c2
blakeedwards823 <blakeedwards823@gmail.com>
2020-04-10 22:26:09 -0400

    updated ternary training procedure and add to experiments directory

--- src/experiments/ternary_train.py
+++ src/experiments/ternary_train.py
@@ -1,9 +1,24 @@
 
-from tqdm import tqdm
+# ternary network training
+import os
 import math
 import numpy as np
+from tqdm import tqdm
 import tensorflow as tf
+from datetime import datetime
+from utils import config_reference as cfg
+import tensorflow.python.keras.backend as K
 # tf.compat.v1.disable_eager_execution()
+
+# write weights to file
+def save_weights(models_dir, model, epoch, total_epochs, val_acc, ternary=False):
+    if ternary:
+        weight_filename = f"ternary_model_{epoch}|{total_epochs}_{format(val_acc, '.5f')}.h5"
+    else:
+        weight_filename = f"model_{epoch}|{total_epochs}_{format(val_acc, '.5f')}.h5"
+    model_path = os.path.join(models_dir, weight_filename)
+    model.save_weights(model_path)
+    return model_path
 
 def bound_policy(w):
     if w < -1.0:
@@ -46,6 +61,17 @@ def train_step(real_x, real_y):
         cnt += 1
     # todo: set ternary_model weights to ternarized model weights
 
+
+# create log directory
+print("Creating logs directory...")
+log_dir = cfg.log_dir
+now = datetime.now()
+now_datetime = now.strftime("%d-%m-%y_%H:%M:%S")
+log_dir = os.path.join(log_dir, "ternary_train_" + cfg.dataset + "_" + now_datetime)
+os.mkdir(log_dir)
+models_dir = os.path.join(log_dir, "model_weights")
+os.mkdir(models_dir)
+
 # Load and pre-process training data
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train = (x_train / 255).reshape((-1, 28, 28, 1))
@@ -57,8 +83,8 @@ y_test = tf.keras.utils.to_categorical(y_test, 10)
 batch_size = 64
 epochs = 50
 weight_init = tf.keras.initializers.RandomUniform(minval=-1.0, maxval=1.0, seed=None)
-optimizer = tf.keras.optimizers.SGD(lr=0.005, decay=1e-6, momentum=0.9, nesterov=True)
-optimizer2 = tf.keras.optimizers.SGD(lr=0.005, decay=1e-6, momentum=0.9, nesterov=True)
+optimizer = tf.keras.optimizers.SGD(lr=0.002, decay=1e-6, momentum=0.9, nesterov=True)
+optimizer2 = tf.keras.optimizers.SGD(lr=0.002, decay=1e-6, momentum=0.9, nesterov=True)
 
 # Build model
 model = tf.keras.models.Sequential()
@@ -85,6 +111,11 @@ for layer in model.layers:
         ternary_model.layers[cnt].set_weights(ws)
     cnt += 1
 
+# Save model json configuration
+model_json = model.to_json()
+with open(os.path.join(log_dir, "model.json"), "w") as json_file:
+    json_file.write(model_json)
+
 print('Before training (FP):\n', model.evaluate(x_test, y_test, verbose=0))
 print('Before training (TN):\n', ternary_model.evaluate(x_test, y_test, verbose=0))
 
@@ -92,6 +123,7 @@ print('Before training (TN):\n', ternary_model.evaluate(x_test, y_test, verbose=
 bat_per_epoch = math.floor(len(x_train) / batch_size)
 for epoch in range(epochs):
     print(f'\n[epoch {epoch+1}]')
+    print(f'lr: {format(K.eval(model.optimizer.lr), ".5f")}')
     for i in tqdm(range(bat_per_epoch)):
         n = i * batch_size
         # Ternarize FP model weights and set ternary model weights
@@ -103,13 +135,14 @@ for epoch in range(epochs):
                 ternary_model.layers[cnt].set_weights(ws)
             cnt += 1
         train_step(x_train[n:n + batch_size], y_train[n:n + batch_size])
-    print('Saving models...')
-    ternary_model.save("ternary_conv_model.h5")
-    model.save("fp_conv_model.h5")
-    print('32 bit model:\n', model.evaluate(x_test, y_test, verbose=0))
-    print('2 bit model:\n', ternary_model.evaluate(x_test, y_test, verbose=0))
-    # print(model.get_weights())
-    # print(ternary_model.get_weights())
+    model.evaluate(x_test, y_test, verbose=0)
+    print('\nEvaluating and saving models...')
+    tn_results = ternary_model.evaluate(x_test, y_test, verbose=0)
+    fp_results = model.evaluate(x_test, y_test, verbose=0)
+    save_weights(models_dir, ternary_model, epoch+1, epochs, tn_results[1], True)
+    save_weights(models_dir, model, epoch+1, epochs, fp_results[1], False)
+    print('32 bit scores:\n', fp_results)
+    print('2 bit scores:\n', tn_results)
 
 print('\nAfter training (FP):\n', model.evaluate(x_test, y_test, verbose=0))
 print('After training (TN):\n', ternary_model.evaluate(x_test, y_test, verbose=0))
